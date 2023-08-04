22df41b45c74cb62816f8c2ced02e1a98cf25674
Caleb Beazel <caleb.beazel@gmail.com>
2023-08-04 17:53:35 -0500

    added fizzbuzz

--- fizz-buzz-lc.py
+++ fizz-buzz-lc.py
@@ -1,3 +1,13 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        if 
+        ls_fb = []
+        for i in range(1,n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ls_fb.append("FizzBuzz") 
+            elif i % 3 == 0:
+                ls_fb.append("Fizz") 
+            elif i % 5 == 0:
+                ls_fb.append("Buzz")
+            else:
+                ls_fb.append(str(i))
+        return ls_fb
